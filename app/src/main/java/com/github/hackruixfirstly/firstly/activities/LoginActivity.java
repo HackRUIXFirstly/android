ea14e7ed7075e098eed2f9b0ba1d05e29ec2d6a6
Trevor Summerfield <tresummerfield@gmail.com>
2015-10-03 20:40:52 -0400

    login button, reformatted some layout

--- app/src/main/java/com/github/hackruixfirstly/firstly/activities/LoginActivity.java
+++ app/src/main/java/com/github/hackruixfirstly/firstly/activities/LoginActivity.java
@@ -2,15 +2,22 @@ package com.github.hackruixfirstly.firstly.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.github.hackruixfirstly.firstly.R;
 
 import java.util.Arrays;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by trevor on 10/3/15.
@@ -20,6 +27,9 @@ public class LoginActivity extends AppCompatActivity {
     CallbackManager callbackManager;
     LoginManager loginManager;
 
+    @Bind(R.id.activity_login_fb_button)
+    TextView loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +38,29 @@ public class LoginActivity extends AppCompatActivity {
 
         loginManager = LoginManager.getInstance();
         callbackManager = CallbackManager.Factory.create();
+
+        loginManager.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                Snackbar.make(loginButton, "Login Cancelled by User", Snackbar.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Snackbar.make(loginButton, error.getMessage(), Snackbar.LENGTH_LONG).show();            }
+            }
+        );
+    }
+
+    @OnClick(R.id.activity_login_fb_button)
+    public void login() {
         loginManager.logInWithReadPermissions(this, Arrays.asList("user_friends"));
     }
 
